48a719cc2ed73c06c7feef4fce167e6d80c8084d
Aanand Kainth <aanand@akainth.me>
2020-07-15 16:02:55 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 8080});
+
+const uidmapping = {};
+
+wss.on('connection', function connection(ws) {
+    console.log("New connection")
+    let identityHasBeenReceived = false;
+    let identity;
+
+    ws.on('message', function incoming(message) {
+        console.debug(message);
+        if (!identityHasBeenReceived) {
+            // Very insecure, we will take the user's identity for who they say they are
+            identity = message;
+            uidmapping[message] = ws;
+            identityHasBeenReceived = true;
+        } else {
+            const payload = JSON.parse(message);
+
+            uidmapping[payload.target].send(JSON.stringify({
+                author: identity,
+                ...message
+            }));
+        }
+    });
+
+    ws.on("close", () => {
+        uidmapping[identity] = null;
+    });
+});
